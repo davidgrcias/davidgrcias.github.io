8a83ee2df088d4478a5deb84845b75fafa1f5346
David G Tech <76923507+davidgrcias@users.noreply.github.com>
2025-06-18 19:20:35 +0700

    gemini model of chatbot

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  define: {
-    "process.env": process.env,
-  },
   server: {
     host: true,
     port: 5174,
